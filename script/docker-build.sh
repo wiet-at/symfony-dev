7e67117273c95161445148fb8349afa84482a921
Stefan Reisinger <dev@stefan-reisinger.at>
2021-08-05 14:39:24 +0200

    clean up apt cache and similar

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-docker build -t 1994rstefan/symfony-dev:latest ./docker/
+docker build -t ghcr.io/1994rstefan/symfony-dev:latest ./docker/
+docker push ghcr.io/1994rstefan/symfony-dev:latest
