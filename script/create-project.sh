9df3539ec956f0156e7ce8f26712617ac5eb9ea7
Stefan Reisinger <dev@stefan-reisinger.at>
2021-08-05 11:17:51 +0200

    fixed create-project script

--- script/create-project.sh
+++ script/create-project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # shellcheck disable=SC2068
-docker run -it --rm -v "$(pwd)":/app --env SYMFONY_DEV_USER="$(id -u)" --env SYMFONY_DEV_GROUP="$(id -g)" 1994rstefan/symfony-dev symfony new $@
+docker run --rm -v "$(pwd)":/app --env SYMFONY_DEV_USER="$(id -u)" --env SYMFONY_DEV_GROUP="$(id -g)" 1994rstefan/symfony-dev symfony new $@
