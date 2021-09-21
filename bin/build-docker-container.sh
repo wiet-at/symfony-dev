dff207e2ca650c52d66c393f2cef5356212ffc0f
Stefan Reisinger <dev@stefan-reisinger.at>
2021-09-21 14:49:09 +0200

    added license

--- bin/build-docker-container.sh
+++ bin/build-docker-container.sh
@@ -7,6 +7,6 @@ bin/create-cli-phar.php
 echo '################################'
 echo
 echo 'Building Docker container'
-docker build -t 1994rstefan/symfony-dev:latest -t ghcr.io/1994rstefan/symfony-dev:latest ./docker/
-docker push ghcr.io/1994rstefan/symfony-dev:latest
+docker build -t ghcr.io/wiet-at/symfony-dev/cli:latest ./docker/
+docker push ghcr.io/wiet-at/symfony-dev/cli:latest
 echo '################################'
--- bin/create-project.sh
+++ bin/create-project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # shellcheck disable=SC2068
-docker run -it --rm -v "$(pwd)":/app --env SYMFONY_DEV_USER="$(id -u)" --env SYMFONY_DEV_GROUP="$(id -g)" ghcr.io/1994rstefan/symfony-dev symfony-dev-cli create-project $@
+docker run -it --rm -v "$(pwd)":/app --env SYMFONY_DEV_USER="$(id -u)" --env SYMFONY_DEV_GROUP="$(id -g)" ghcr.io/wiet-at/symfony-dev/cli symfony-dev-cli create-project $@
